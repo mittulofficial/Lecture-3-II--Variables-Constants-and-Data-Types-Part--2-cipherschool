5a4a16dd83f055145f02849c92954375655e7a2d
MITTUL KUMAR <kmittul2@gmail.com>
2023-06-26 15:53:44 +0530

    difference between a+b and "a+b" in court

--- lecture3 cipherschool/main.cpp
+++ lecture3 cipherschool/main.cpp
@@ -10,11 +10,9 @@ using namespace std;
 int main()
 {
    //declare 2 variables and then assign them values and then add them
-    int  a,b;
-    a=3;
-    b=5;
-    int c;
-    c=a+b;
-    cout<<"a+b";
+    int a=3;
+    int b=5;
+    cout<<a+b<<endl<<"a+b";
+    
     
 }
