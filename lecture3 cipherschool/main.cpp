1c4a073f26073c39bbe09dc098e21e7b7ac221b0
MITTUL KUMAR <kmittul2@gmail.com>
2023-06-26 15:50:17 +0530

    making variable

--- lecture3 cipherschool/main.cpp
+++ lecture3 cipherschool/main.cpp
@@ -6,9 +6,15 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+int main()
+{
+   //declare 2 variables and then assign them values and then add them
+    int  a,b;
+    a=3;
+    b=5;
+    int c;
+    c=a+b;
+    cout<<c;
+    
 }
